ad484724920e233fed0d5ef2adb2bba8d529f3a3
Alexis <alexis.lapeze@limayrac.fr>
2017-01-20 11:09:42 +0100

    alexis

--- includes/header.php
+++ includes/header.php
@@ -1,35 +1,12 @@
-<head>
-		<title>Maison des ligues - M2L</title>
-		<link rel="icon" type="image/png" href="images/m2lLogo.png"/>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" type="text/css" href="assets/css/main.css" />
-		<link rel="stylesheet" type="text/css" href="assets/css/css_custom.css" />
-		<link rel="stylesheet" type="text/css" href="assets/css/unslider.css" />
-	</head>
-	<body>
-		<div id="page-wrapper">
-			<!-- Header -->
-			<div id="header">
-			<!-- Logo -->
-				<h1><a id="logo">M2L <em>Maison des ligues</em></a></h1>
-			<!-- Navbar -->
-				<nav id="nav">
-					<ul>
-						<li class="current"><a href="index.html">Accueil</a></li>
-						<li>
-							<a href="#">Maison des Ligue</a>
-							<ul>
-								<li><a href="#ligue">Ligue</a></li>
-								<li><a href="#mission">Mission</a></li>
-								<li><a href="#correspondance">Correspondance des bureaux et des ligues</a></li>
-								<li><a href="#financement">Financement</a></li>
-								<li><a href="#service">Services proposés</a></li>
-								<li><a href="#contact">Contact</a></li>
-							</ul>
-						</li>
-						<li><a href="Rugby.html">Rugby à XV</a></li>
-						<li><a href="Volley.html">Volley</a></li>
-					</ul>
-				</nav>
-			</div>
+<?php 
+	if (basename($_SERVER['PHP_SELF'] == "index.php")) {
+		echo "<title>Maison des ligues - M2L</title>";
+	}
+ ?>
+
+<link rel="icon" type="image/png" href="images/m2lLogo.png"/>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" type="text/css" href="assets/css/main.css" />
+<link rel="stylesheet" type="text/css" href="assets/css/css_custom.css" />
+<link rel="stylesheet" type="text/css" href="assets/css/unslider.css" />
