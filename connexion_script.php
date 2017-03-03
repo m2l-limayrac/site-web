7acad34924e0ffab0d68f617020d9ad1c3f1302e
Alexis <alexis.lapeze@limayrac.fr>
2017-03-03 11:27:54 +0100

    creation fonction connexion

---
+++ connexion_script.php
@@ -0,0 +1,49 @@
+<?php
+function connect ($pseudo, $MotDePasse){
+            // connexion BD
+            $mysqli = mysqli_connect("localhost","root","","m2l");
+            //vérification connexion 
+            if(!$mysqli)
+            {
+                echo "Erreur de connexion à la base de données.";
+            } 
+            else 
+            {
+                // requete SQL
+                $Requete = mysqli_query($mysqli,"SELECT * FROM user WHERE pseudo = '".$pseudo."' AND mdp = '".$MotDePasse."'");
+                //on est senser avoir 1 sin on a 1 alors c qu'on a un resultat
+                    if(mysqli_num_rows($Requete) == 0) // si pas de valeur dans la DB
+                    {
+                    
+                      echo "<script>alert(\"Identifiant ou Mot de Passe Incorrect\")</script>"; 
+
+                    }   
+                    else 
+                    {
+                    
+                        $test = "SELECT id_ligue FROM user WHERE pseudo = '$pseudo'";
+                        $result = mysqli_query($mysqli, $test);
+                        
+                            if (mysqli_num_rows($result) > 0)
+                            {
+                                while ($row = mysqli_fetch_assoc($result)){
+                                    if($row["id_ligue"] == 1)
+                                {
+                                    session_start();
+                                    $_SESSION['login'] = $pseudo;
+                                    $_SESSION['ligue'] = $row["id_ligue"];
+                                    header('Location: ./Rugby.php');
+                                    exit();
+                                      
+                                }  else 
+                                {
+                                    echo "<script>alert(\"Vous tentez de vous connecter a la mauvaise ligue!!\")</script>";
+                                }
+                                }                                
+                            }
+                    }
+            }    
+}
+
+
+?>
